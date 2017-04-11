131d2b971bf8d716f8b837979391fa3ee73a4b85
exoslav <m.nikl@seznam.cz>
2017-04-11 23:21:12 +0200

    eslint added with .eslintrc.json file (basic configuration for eslint)

--- app/index.js
+++ app/index.js
@@ -1,14 +1,14 @@
 import express from 'express'
-import * as admin from "firebase-admin";
+import * as admin from "firebase-admin"
 import * as serviceAccount from './service-account.json'
 
 const firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://demigod-3bd86.firebaseio.com"
-});
+})
 
-const db = defaultApp.database()
-const auth = defaultApp.auth()
+const db = firebase.database()
+const auth = firebase.auth()
 const app = express()
 
 app.get('/', (req, res) => {
